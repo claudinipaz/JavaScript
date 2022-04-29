e7aeb5f4296e83b023ffda92a71bd28a2ca2b79a
claudia <claudinipaz2@gmail.com>
2022-04-29 10:50:39 -0300

    Este tp es para JavaScipt

---
+++ variables.js
@@ -0,0 +1,41 @@
+let nombre= "Maximiliano";
+console.log(nombre);
+let apellido= "Vega";
+console.log(apellido);
+let edad= "35";
+  console.log(edad);
+
+const ciudad1="CORDOBA";
+console.log(ciudad1);
+const ciudad2="CHACO";
+console.log(ciudad2);
+const ciudad3="ROSARIO";
+console.log(ciudad3);
+const ciudad4="MENDOZA";
+console.log(ciudad4);
+
+let nombre1="Claudia";
+console.log(nombre1);
+let apellido1="Paz Blanco";
+console.log(apellido1);
+
+let persona= "Daniel Maximiliano";
+let domicilio= "avenida siempre viva 742"
+let pais= "Argentina";
+let nacimiento="06-'9-85";
+let carnet= persona+" "+domicilio+" "+pais+" "+nacimiento;
+console.log(carnet);
+
+
+let entrada = prompt("INGRESAR UN  NOMBRE");
+let primerNombre = "Daniel";
+if (entrada == primerNombre) {
+    alert("Bienvenido!!");
+} else {
+    alert("No podes entrar!!");
+}
+
+let uno="hola";
+let dos="mundo";
+let union= uno+" "+dos;
+document.write(union);
